87ee68eb3e73d9df8c6d632c73a2432e97c0f5e7
Omerbhatti <Omerbhatti34@gmail.com>
2021-01-22 19:57:31 +0500

    Daraz.pk Node Scrapper

---
+++ daraz.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+const utils = require('./utils');
+
+(async () => {
+    
+    url="https://www.daraz.pk/laptops/?spm=a2a0e.home.cate_1.4.35e349373bwClC";
+    totalPages =4;
+    const browser = await puppeteer.launch( {'headless' : true} );
+    console.log("browser launched...");
+
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(0);
+    await page.goto(url, {waitUntil: 'networkidle2'});
+    console.log("page opened...");
+
+    var results = [];
+    for(let currentPage = 0; currentPage < totalPages ; currentPage++)
+    {
+        console.log("-------------Scrapping PAGE "+(currentPage+1)+"-----------------");
+        results = results.concat(await extractedEvaluateCall(page));
+        await page.click("li[title='Next Page']>.ant-pagination-item-link");
+        await page.reload();
+    }
+    utls.StoreJSON(results,'./data.json');
+
+    await browser.close();
+})();
+
+async function extractedEvaluateCall(page) 
+{
+    return page.evaluate(() => {
+        let data = [];
+        let price = Array.from(document.querySelectorAll('span[class="c13VH6"]')).map((val)=>val.innerHTML);
+        let name =  Array.from(document.querySelectorAll('div[class="c16H9d"] a')).map((val)=>val.innerHTML);
+        
+        for(let i=0;i<price.length;i++)
+        {
+            let Price = price[i];
+            let Name = name[i];
+            data.push({ Price, Name });
+        }
+        return data;
+    });
+}
---
+++ utils.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const request = require('request');
+
+exports.download = (url, path, callback)=>{
+    request.head(url, (err, res, body) => {
+        request(url)
+            .pipe(fs.createWriteStream(path))
+            .on('close', callback);
+    });
+}
+
+exports.StoreJSON = (myArray,path)=>{
+    content=JSON.stringify(myArray,null,3);
+    fs.writeFile(path,content,
+        function (err) {
+            if (err) {
+                console.error('File Was Not Created.');
+            }
+        }
+    );
+    console.log('data.json File Created.');
+}
