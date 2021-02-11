ef32bd8542604887b1edb8acbdca8c0190dbe21c
Omerbhatti <Omerbhatti34@gmail.com>
2021-02-11 17:51:48 +0500

    added proper folder names

--- Manganelo Scrapper/manga.js
+++ Manganelo Scrapper/manga.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer');
 const utils = require('../utils');
+const fs = require('fs');
 
 (async () => {
     
-    url=`https://manganelo.link/manga/shingeki-no-kyojin/shingeki-no-kyojin-chapter-127/`;
+    url=`https://manganelo.link/manga/ultimate-scheming-system/chapter-1/`;
     
     const browser = await puppeteer.launch( {'headless' : true} );
     console.log("browser launched...");
@@ -14,24 +15,43 @@ const utils = require('../utils');
     console.log("URL Loaded...");
 
     var results = [];
+    var heading =[];
 
-    results = await extractedEvaluateCall(page);
-    console.log(results);
-    
-    for (let index = 0; index < results.length; index++) 
+    //extracting data
+    results = await extractImages(page);
+    heading = await extractHeading(page);
+
+    //extract manga details
+    var chapterNo = heading[heading.length - 1];
+    var dashIndex = utils.searchInArray("-",heading);
+    var mangaName = utils.concatStringsFromArray(0,dashIndex,heading);
+
+    //create folders
+    fs.mkdir(`./${mangaName}/Chapter${chapterNo}`,{recursive:true},(err) => {
+        if (err) throw err;
+    });
+
+    for (let i = 0; i < results.length; i++) 
     {
-        utils.download(results[index],`./images/${index}.jpg`,()=>{
-            console.log(index+ ".jpg done");
+        utils.download(results[i],`./${mangaName}/Chapter${chapterNo}/${i}.jpg`,()=>{
+            console.log("downloading "+i+ ".jpg");
         });
     }
-
     await browser.close();
 })();
 
-async function extractedEvaluateCall(page) 
+async function extractImages(page)
 {
     return page.evaluate(() => {
         let name = Array.from(document.querySelectorAll(".page-break img")).map(link=>link.src);
         return name;
+    });
+}
+
+async function extractHeading(page)
+{
+    return page.evaluate(() => {
+        let heading = document.getElementById("chapter-heading").innerHTML.split(" ");
+        return heading;
     });
 }
--- utils.js
+++ utils.js
@@ -3,9 +3,14 @@ const request = require('request');
 const fetch = require('node-fetch');
 
 exports.download = async function(url, filename, callback){
-    const response = await fetch(url);
-    const buffer = await response.buffer();
-    fs.writeFile(filename, buffer, callback);
+    try{
+        const response = await fetch(url);
+        const buffer = await response.buffer();
+        fs.writeFile(filename, buffer, callback);
+    }
+    catch(e){
+        console.log(e);
+    }
 };
 
 exports.StoreJSON = (myArray,path)=>{
@@ -30,4 +35,22 @@ exports.ArrayToTxt = (myArray,path)=>{
         }
     );
     console.log(`File Created at ${path}`);
+}
+
+exports.concatStringsFromArray = (start,end,array)=> {
+    let str="";
+    for (let i = start; i < end; i++) 
+    {
+        str = str.concat(array[i]);
+    }
+    return str;
+}
+
+exports.searchInArray = (str, strArray)=> {
+    for (var j=0; j<strArray.length; j++) 
+    {
+        if (strArray[j].match(str))
+            return j;
+    }
+    return -1;
 }
